fe5b5a46dc2e57c8a362f541e17bb168876e6cdb
JinselW <2552685877@qq.com>
2026-01-30 17:12:56 +0800

    提交练习

---
+++ python/manage list.py
@@ -0,0 +1,21 @@
+planed_places = ['xinjiang', 'xian', 'haerbing', 'dali', 'nuowei']
+
+print(planed_places)
+
+print(sorted(planed_places))
+print(planed_places)
+
+print(sorted(planed_places, reverse = True))
+print(planed_places)
+
+planed_places.reverse()
+print(planed_places)
+
+planed_places.reverse()
+print(planed_places)
+
+planed_places.sort()
+print(planed_places)
+
+planed_places.sort(reverse = True)
+print(planed_places)
